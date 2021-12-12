14625ffd47d5fa33b5908b83e8e2197014ab0a27
Jacob <kamstrup1993@gmail.com>
2021-12-12 17:49:27 +0100

    added counter - video and frontpage picture

--- src/main/java/databois/christiansoe/errors/NotFoundException.java
+++ src/main/java/databois/christiansoe/errors/NotFoundException.java
@@ -1,4 +1,5 @@
-package sem3.rest_js.demo.errors;
+package databois.christiansoe.errors;
+
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
